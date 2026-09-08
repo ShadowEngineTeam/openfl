0ca9c246f2c5a818651ab956f35324e790f8e5f6
Homura <egzozu.be.bas@gmail.com>
2026-09-08 13:18:08 +0300

    Update CairoTextField.hx

--- src/openfl/display/_internal/CairoTextField.hx
+++ src/openfl/display/_internal/CairoTextField.hx
@@ -184,7 +184,7 @@ class CairoTextField
 		{
 			options.hintStyle = CairoHintStyle.SLIGHT;
 			options.hintMetrics = CairoHintMetrics.OFF;
-			options.antialias = CairoAntialias.SUBPIXEL;
+			options.antialias = CairoAntialias.GOOD;
 		}
 
 		cairo.fontOptions = options;
