92168909774e84933244091055d643d525c00302
Homura <egzozu.be.bas@gmail.com>
2026-09-11 17:25:06 +0300

    Update Shader.hx

--- src/openfl/display/Shader.hx
+++ src/openfl/display/Shader.hx
@@ -615,13 +615,13 @@ class Shader
 			extensions += "#extension GL_OES_standard_derivatives : enable\n";
 		}
 
-		if (OpenGLRenderer.__drawBuffersARB)
-		{
-			extensions += "#extension GL_ARB_draw_buffers : enable\n";
-		}
-		else if (OpenGLRenderer.__drawBuffersEXT == null)
+		if (OpenGLRenderer.__drawBuffersEXT)
 		{
 			extensions += "#extension GL_EXT_draw_buffers : enable\n";
+		}
+		else if (OpenGLRenderer.__drawBuffersARB)
+		{
+			extensions += "#extension GL_ARB_draw_buffers : enable\n";
 		}
 
 		// #version must be the first directive and cannot be repeated,
